08dfac4926082ef79b1ce8ec0057bf2e4e968319
Nirmal Bhandari <vhandarinirmal@gmail.com>
2024-06-14 17:27:35 -0500

    fixed error with prod

--- app/Landing/page.tsx
+++ app/Landing/page.tsx
@@ -1,17 +1,8 @@
 
-import '@/app/ui/Landing/landing.css';
-
 export default async function Page() {
     
   return ( 
     <main className=' w-screen h-screen'>
-
-    {/* <section className="footing">
-     
-                    <h1 >Footeerrrr </h1>
-                   
-    </section> */}
-    {/* <first/> */}
   </main>
   
   );
